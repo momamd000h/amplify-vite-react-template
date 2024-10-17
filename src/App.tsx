4a669060f1b5c87a11e0b52d5dda0c9580d29ebb
momamd000h <mo.mamd000h@gmail.com>
2024-10-17 05:20:41 +0300

    Your commit message here

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 import { useAuthenticator } from "@aws-amplify/ui-react";
@@ -7,13 +7,9 @@ import "./App.css"; // Ensure your styles are applied
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
   const { user, signOut } = useAuthenticator();
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
+    client.models.Todo.observeQuery().subscribe({});
   }, []);
 
   return (
